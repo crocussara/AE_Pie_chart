b041173fe837d17346985ebb342768fbb3a91c80
crocussara <crocus.sara.1@gmail.com>
2023-08-17 13:55:58 +0200

    last checks

--- test77.jsx
+++ test77.jsx
@@ -1,8 +1,15 @@
+// Pie Chart script, to create said charts. Adobe Extendscript, to use it with Adobe After Effects
+
+// Sara Safrany https://github.com/crocussara 
+// Portfolio: https://www.behance.net/safranysara
+// Created: 2023
+// Tested on Adobe AE CC 2020 and AE CC 2023
+
 (function(thisObj) {
     scriptPieChart(thisObj)
 
     function scriptPieChart(thisObj) {
-        var myWindow = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Pie Chart", undefined);// Create a new window
+        var myWindow = (thisObj instanceof Panel) ? thisObj : new Window("palette", "Pie Chart", undefined);
         // won't work if comp is not defined
 
         var comp = app.project.activeItem;
@@ -66,7 +73,6 @@
         // Create a panel to hold the text input fields
         var textInputPanel = myWindow.add("group");
         textInputPanel.orientation = "row";
-
         //these are needed for every input you want to add with a click
         var sliceNames = [];
         var slicePercentages = [];
@@ -74,8 +80,8 @@
         // Define the button click event handler for the Submit button
         submitButton.onClick = function() {
             var numValue1 = Number(sliceNumInput.text);
-            if (isNaN(numValue1) || numValue1 < 1 || numValue1 > 20) {
-                alert("Invalid input! Please enter a number between 1 and 20.");
+            if (isNaN(numValue1) || numValue1 < 1 || numValue1 > 50) {
+                alert("Invalid input! Please enter a number between 1 and 50.");
             } else {
                 createTextInputFields(numValue1);   
             };
@@ -89,35 +95,35 @@
             }
             
             //create named panels for the different elements
-            var sliceNamePanel = textInputPanel.add("panel", undefined, "name of the slice");
+            var sliceNamePanel = textInputPanel.add("panel", undefined, "Name of the slice");
             sliceNamePanel.orientation = "column";
 
-            var slicePercentagePanel = textInputPanel.add("panel", undefined, "percentage");
+            var slicePercentagePanel = textInputPanel.add("panel", undefined, "Percentage");
             slicePercentagePanel.orientation = "column";
 
             for (var i = 0; i < count; i++) {
 
-                    //add as manz sliceNames as many input value
-                    var sliceName = sliceNamePanel.add("edittext", undefined, "", {
+                //add as many sliceNames as many input value
+                var sliceName = sliceNamePanel.add("edittext", undefined, "", {
                     multiline: false,
                     enterKeySignalsOnChange: true,
                     justify: "center"
                 });
-                sliceName.characters = 15; // Set the maximum number of characters for each input field
+                sliceName.characters = 15; 
                 sliceNames.push(sliceName); // push slice name into slice names
             
                 var slicePercentage = slicePercentagePanel.add("edittext", undefined, "", {
                     multiline: false,
                     enterKeySignalsOnChange: true,
                     justify: "center",
-                    numericOnly: true // Only allows numeric input
+                    // ! Only allows numeric input, but heck it doesn't really wwork. 
+                    numericOnly: true 
                 });
-                slicePercentage.characters = 5; // Set the maximum number of characters for each input field
+                slicePercentage.characters = 5; 
                 slicePercentages.push(slicePercentage);
 
             }
             
-
             // Fit the window size to accommodate the text input fields
             myWindow.layout.layout(true);
             myWindow.layout.resize();    
@@ -158,17 +164,17 @@
                             break; // No need to continue checking if we found an empty element
                         };
                     };
-
+                    // making sure that every neccessary field is filled and there are o alphabets where only numbers can be
                     if ( isNaN(numValue2) || isNaN(numValue3) ||isNaN(sum) || numValue4 == true || numValue5 == true || pieName.text === "" || sizeNumInput.text === "" || textInputPanel.children.length < 1 ){
                         alert("Invalid input! Check if you filled out everything correctly!");
                      } else {
-
+                        // in the first code snippet i used a checkbox to see if the pie chart is 3D or not. Here i push the correct true or false values based on the dropdown menu (is or isn't)
                         if (dropDown1.selection.index == 0) {
                             threeDBoolean = false;
                         } else if (dropDown1.selection.index == 1) {
                             threeDBoolean = true;
                         }   
-            
+                        // doing some quick checks and if the value of all percantage values is more than a 100 then alert and abort, if less then just alert but continue, if it is 100 then just proceed
                         if (sum < 100) {
                             alert("Be aware, the sum of all the percentage values is less than 100!");
                             if (threeDBoolean == true) {
@@ -191,18 +197,17 @@
 
         function createPieChart() {
 
-            // hard coded null names, if ever improved add null names here
-                    //var cntrlNullName = [" - CNTRL - slices" , " - CNTRL - colors" , " - CNTRL - size & extrusion" , " - CNTRL - position & rotation"];
+                    // * hard coded null names, if ever improved add null names here
                     var cntrlNullName = [" - CNTRL - slices" , " - CNTRL - colors" , " - CNTRL - size" , " - CNTRL - position & rotation"];
                     if (threeDBoolean == true) { 
                         cntrlNullName[2] = cntrlNullName[2] +' & extrusion';
                     };
 
-                    //updating the null names with the main pie name input
+                    //updating the null names with the name of the Pie chart (var pieName)
                     for (var k = 0; k < cntrlNullName.length; k++) {
                         cntrlNullName[k] = pieName.text + cntrlNullName[k];
                     };
-                    // loop for creating the control nulls, and creating an array what can be later called when adding sliders to differnt nulls
+                    // loop for creating the control nulls, and creating an array what can be later called when adding sliders to differnt nulls. Changing the label color from the basic red
                     var cntrlNullS = [];
                     for (var l = 0; l < cntrlNullName.length; l++) {
                         var cntrlNull = comp.layers.addNull();
@@ -211,7 +216,7 @@
                         cntrlNullS.push(cntrlNull);
                     };
                 
-                    // ! putting the expression for the trim paths together with the inputs
+                    // * putting the expression for the trim paths together with the inputs
                     // Initialize an array starting with 0. This array will have one more length than your input
                     var trimPathExpression = [0]; 
                     // the 0 th element exception
@@ -226,7 +231,7 @@
                         previous = next;               
                     };
 
-                    // ! vars and maths for the size of the pie
+                    //* ----- CNTRL - size & extrusion
                     var size = 'Size';
                     var extrusion = 'Extrusion';
                     var effectsPropertySize = cntrlNullS[2].property("ADBE Effect Parade");
@@ -235,9 +240,7 @@
                     // set slider value , and add expression which devides the input number by 2
                     sliderSize.slider.setValue(sizeNumInput.text);
                     sliderSize.slider.expression = 'effect("' + size + '")(1)/2';
-
-
-
+                    // only add the extrusion slider if a 3D pie is created
                     if (threeDBoolean == true) {
                         var sliderExtrusion = effectsPropertySize.addProperty("ADBE Slider Control");
                         sliderExtrusion.name = extrusion;
@@ -245,7 +248,7 @@
                     };
 
 
-                    //! position to rotation cntrl to 3d
+                    //* ----- CNTRL - size & extrusion
                     cntrlNullS[3].threeDLayer = threeDBoolean;
                     if (threeDBoolean == true) {
                         var threeDRotationValue = 125;
@@ -255,7 +258,7 @@
 
 
             
-                    
+                    // setting up the vars and expression for the shape layers
                     for (var j = 0; j < sliceNames.length; j++) {
                         var sliceNamePlusNumber = j+1 + '.- ' + sliceNames[j].text;
                         var colorSelection = sliceNamePlusNumber + ' - color';
@@ -270,7 +273,8 @@
                         var myTrimStart = trimPathExpression[j];
                         var myTrimEnd = trimPathExpression[j+1];
                         var extrusionValue = 'thisComp.layer("' + cntrlNullName[2] + '").effect("' + extrusion + '")("Slider")+thisComp.layer("' + cntrlNullName[2] + '").effect("' + extrusionSelection + '")("Slider")';
-                            
+                         
+                    //* SLICE layers
                         var myShapeLayer = comp.layers.addShape();
                         myShapeLayer.label = 9;
                         myShapeLayer.name = sliceNamePlusNumber;
@@ -296,40 +300,37 @@
                         // Trim End
                         myTrim.end.expression = myTrimEnd;
 
-                        //todo if 3d is checked, for the shape layers
+                        //if 3d is checked, for the shape layers
                         if (threeDBoolean == true) {
                             var threeDSide = myShapeGroup.property("ADBE Vector Materials Group");
                             threeDSide.addProperty("ADBE Vec3D Side RGB");
                             threeDSide.property("ADBE Vec3D Side RGB").expression = myStrokeSideColor;
-                            //! Extrude the shape layer
                             myShapeLayer.property("ADBE Extrsn Options Group").property("ADBE Extrsn Depth").expression = extrusionValue;
                             
                         };
 
-                        // todo add slider for - CNTRL - slices / - CNTRL - slices = cntrlNullS[0]
+                        //* ----- CNTRL - slices (cntrlNullS[0])
                         var effectsProperty = cntrlNullS[0].property("ADBE Effect Parade");
                         var sliderSize = effectsProperty.addProperty("ADBE Slider Control");
-                        // rename slider 1
                         sliderSize.name = sliceNamePlusNumber;
-                        // set slider value
                         sliderSize.slider.setValue(slicePercentages[j].text);
 
 
-                        // todo add slider for - CNTRL - size & extrusion / for each slice, so the slice can be highlighted
+                        //* ----- CNTRL - size & extrusion / a slider for each slice, so the slice can be highlighted
                         var sliderSize = effectsPropertySize.addProperty("ADBE Slider Control");
                         sliderSize.name = sizeSelection;
-                        // todo size and extrusion cntrl add extrusion sliders if 3d is thicked
+                        // if 3D add more sliders
                         if (threeDBoolean == true) {
                             var sliderExtrusion = effectsPropertySize.addProperty("ADBE Slider Control");
                             sliderExtrusion.name = extrusionSelection;
                         };
 
-                        //todo color picker 
+                        //* ----- CNTRL - color (cntrlNullS[1]) 
                         var effectsPropertyColor = cntrlNullS[1].property("ADBE Effect Parade");
                         var sliderColor = effectsPropertyColor.addProperty("ADBE Color Control");
                         sliderColor.name = colorSelection;
                         sliderColor.color.setValue(colorValue);
-                        //todo side color picker , the same color but darkened
+                        // if 3D, side color picker , the same color but darkened
                         if (threeDBoolean == true) {
                             var effectsPropertyColor = cntrlNullS[1].property("ADBE Effect Parade");
                             var sliderColor = effectsPropertyColor.addProperty("ADBE Color Control");
